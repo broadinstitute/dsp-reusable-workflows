49df4365d65c8d50774ecca17adbd449cb54573d
Saloni Shah <salonipshah11@gmail.com>
2023-10-20 13:11:34 -0400

    PR feedback

--- e2e-test/app_helper.py
+++ e2e-test/app_helper.py
@@ -29,8 +29,10 @@ def create_app(workspace_id, leo_url, app_type, access_scope, azure_token):
 # GET APP PROXY URL FROM LEO
 def poll_for_app_url(workspaceId, app_type, proxy_url_name, azure_token, leo_url):
     leo_get_app_api = f"{leo_url}/api/apps/v2/{workspaceId}?includeDeleted=false"
-    headers = {"Authorization": "Bearer " + azure_token,
-               "accept": "application/json"}
+    headers = {
+        "Authorization": "Bearer " + azure_token,
+        "accept": "application/json"
+    }
 
     # prevent infinite loop
     poll_count = 20 # 30s x 20 = 10 min
@@ -38,12 +40,11 @@ def poll_for_app_url(workspaceId, app_type, proxy_url_name, azure_token, leo_url
     while poll_count > 0:
         response = requests.get(leo_get_app_api, headers=headers)
         assert response.status_code == 200, f"Error fetching apps from Leo: ${response.text}"
-        logging.info(f"Successfully retrieved details for {app_type} app")
         response = json.loads(response.text)
 
         # Don't run in an infinite loop if you forgot to start the app/it was never created
         if app_type not in [item['appType'] for item in response]:
-            logging.error(f"{app_type} not found in apps, has it been started?")
+            logging.warn(f"{app_type} not found in apps, has it been started?")
             return ""
         for entries in response:
             if entries['appType'] == app_type:
@@ -57,6 +58,7 @@ def poll_for_app_url(workspaceId, app_type, proxy_url_name, azure_token, leo_url
                     logging.error(f"{app_type} proxyUrls not found: {entries}")
                     return ""
                 else:
+                    logging.info(f"{app_type} is in READY state")
                     return entries['proxyUrls'][proxy_url_name]
         poll_count -= 1
 
