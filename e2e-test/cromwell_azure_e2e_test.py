967e95f8287dc8e36c4392f5d994e4c9e5802653
Janet Gainer-Dewar <jdewar@broadinstitute.org>
2023-10-20 15:00:58 -0400

    Wait between launching two workflow apps

--- e2e-test/cromwell_azure_e2e_test.py
+++ e2e-test/cromwell_azure_e2e_test.py
@@ -100,15 +100,18 @@ def main():
     found_exception = False
     
     # Sleep timers for various steps in the test
-    workflow_run_sleep_timer = 60 * 5
+    inter_app_creation_timer = 60 * 1
     provision_sleep_timer = 60 * 5
+    workflow_run_sleep_timer = 60 * 5
     workflow_status_sleep_timer = 60 * 2
     
     try:
         (workspace_id, workspace_name) = create_workspace(billing_project_name, bearer_token, rawls_url)
-        create_app(workspace_id, leo_url, 'CROMWELL_RUNNER', 'USER_PRIVATE', bearer_token)
+        create_app(workspace_id, leo_url, 'WORKFLOWS_APP', 'WORKSPACE_SHARED', bearer_token)
+        time.sleep(inter_app_creation_timer)
+        create_app(workspace_id, leo_url, 'CROMWELL_RUNNER_APP', 'USER_PRIVATE', bearer_token)
         time.sleep(provision_sleep_timer) # Added an sleep here to give the workspace time to provision and app to start
-        app_url = poll_for_app_url(workspace_id, 'CROMWELL_RUNNER', 'cromwell-runner', bearer_token, leo_url)
+        app_url = poll_for_app_url(workspace_id, 'CROMWELL_RUNNER_APP', 'cromwell-runner', bearer_token, leo_url)
 
         # This chunk of code only executes one workflow
         # Would like to modify this down the road to execute and store references for multiple workflows
