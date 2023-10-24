f3b4f540ada6e05d8591271fc1fff5cfa4c60561
Janet Gainer-Dewar <jdewar@broadinstitute.org>
2023-10-24 11:57:35 -0400

    Handle token consistently, fix error

--- e2e-test/cromwell_azure_e2e_test.py
+++ e2e-test/cromwell_azure_e2e_test.py
@@ -16,14 +16,14 @@ logging.basicConfig(
     datefmt=LOG_DATEFORMAT,
 )
 
-bearer_token = os.environ['BEARER_TOKEN']
+token = os.environ['BEARER_TOKEN']
 bee_name = os.environ['BEE_NAME']
 billing_project_name = os.environ['BILLING_PROJECT_NAME']
 
 rawls_url = f"https://rawls.{bee_name}.bee.envs-terra.bio"
 leo_url = f"https://leonardo.{bee_name}.bee.envs-terra.bio"
 
-def perform_get(endpoint):
+def perform_get(endpoint, bearer_token):
     headers = {"Authorization": f'Bearer {bearer_token}',
               "accept": "application/json"}
     response = requests.get(endpoint, headers=headers)
@@ -32,7 +32,7 @@ def perform_get(endpoint):
         raise Exception(f'{response.status_code} - {msg}\n{response.text}')
     return response.json()
 
-def submit_hello_world_to_cromwell(app_url, workflow_test_name):
+def submit_hello_world_to_cromwell(app_url, workflow_test_name, bearer_token):
     absolute_file_path = os.path.dirname(__file__)
     workflow_source_path = os.path.join(absolute_file_path, './resources/cromwell/hello.wdl')
     workflow_inputs_path = os.path.join(absolute_file_path, './resources/cromwell/hello.inputs')
@@ -55,42 +55,18 @@ def submit_hello_world_to_cromwell(app_url, workflow_test_name):
             logging.debug(response.json())
             return response.json()
         
-# def get_workflow_status(app_url, workflow_id):
-#     workflow_endpoint = f'{app_url}/api/workflows/v1/{workflow_id}/status'
-#     logging.info(f"Fetching workflow status for {workflow_id}")
-#     return perform_get(workflow_endpoint)
-
-# def get_workflow_metadata(app_url, workflow_id):
-#     workflow_endpoint = f'{app_url}/api/workflows/v1/{workflow_id}/metadata'
-#     logging.info(f"Fetching workflow metadata for {workflow_id}")
-#     return perform_get(workflow_endpoint)
-
 def get_workflow_status(app_url, workflow_id, bearer_token):
     workflow_endpoint = f'{app_url}/api/workflows/v1/{workflow_id}/status'
-    headers = {"Authorization": f'Bearer {bearer_token}',
-              "accept": "application/json"}
     logging.info(f"Fetching workflow status for {workflow_id}")
-    response = requests.get(workflow_endpoint, headers=headers)
-    if(response.status_code != 200):
-        msg = f"Error fetching workflow metadata for {workflow_id}"
-        raise Exception(f'{response.status_code} - {msg}\n{response.text}')
-    logging.debug(response.json())
-    return response.json()
+    return perform_get(workflow_endpoint, bearer_token)
 
 def get_workflow_metadata(app_url, workflow_id, bearer_token):
     workflow_endpoint = f'{app_url}/api/workflows/v1/{workflow_id}/metadata'
-    headers = {"Authorization": f'Bearer {bearer_token}',
-              "accept": "application/json"}
     logging.info(f"Fetching workflow metadata for {workflow_id}")
-    response = requests.get(workflow_endpoint, headers=headers)
-    if(response.status_code != 200):
-        msg = f"Error fetching workflow metadata for {workflow_id}"
-        raise Exception(f'{response.status_code} - {msg}\n{response.text}')
-    logging.debug(response.json())
-    return response.json()
+    return perform_get(workflow_endpoint, bearer_token)
 
 # workflow_ids is a deque of workflow ids
-def get_completed_workflow(app_url, workflow_ids, max_retries=4, sleep_timer=60 * 2):
+def get_completed_workflow(app_url, workflow_ids, bearer_token, max_retries=4, sleep_timer=60 * 2):
     success_statuses = ['Succeeded']
     throw_exception_statuses = ['Aborted', 'Failed']
     
@@ -100,16 +76,16 @@ def get_completed_workflow(app_url, workflow_ids, max_retries=4, sleep_timer=60 
             raise Exception(f"Workflow(s) did not finish running within retry window ({max_retries} retries)")
         
         workflow_id = workflow_ids.pop()
-        workflow_status = get_workflow_status(app_url, workflow_id, bearer_token)
-        logging.info(f"Status response is: {workflow_status}")
-        workflow_status = workflow_status['status']
+        workflow_status_json = get_workflow_status(app_url, workflow_id, bearer_token)
+        logging.info(f"Status response is: {workflow_status_json}")
+        workflow_status = workflow_status_json['status']
 
         if(workflow_status in throw_exception_statuses):
             metadata = get_workflow_metadata(app_url, workflow_id, bearer_token)
             logging.info(metadata)
             raise Exception(f"Exception raised: Workflow {workflow_id} reporting {workflow_status} status")
         if workflow_status in success_statuses:
-            logging.info(f"{workflow_id} finished running. Status: {workflow_status['status']}")
+            logging.info(f"{workflow_id} finished running. Status: {workflow_status}")
         else:
             workflow_ids.appendleft(workflow_id)
             current_running_workflow_count += 1
@@ -126,7 +102,7 @@ def get_completed_workflow(app_url, workflow_ids, max_retries=4, sleep_timer=60 
                 time.sleep(sleep_timer)
     logging.info("Workflow(s) submission and completion successful")
         
-def test_cleanup(workspace_namespace, workspace_name):
+def test_cleanup(workspace_namespace, workspace_name, bearer_token):
     try:
         delete_workspace(workspace_namespace, workspace_name, rawls_url, bearer_token)
         logging.info("Workspace cleanup complete")
@@ -135,7 +111,7 @@ def test_cleanup(workspace_namespace, workspace_name):
     except Exception as e:
         logging.info("Error cleaning up workspace, test script will continue")
         logging.info(f'Exception details below:\n{e}')
-
+        
 def main():
     workspace_name = ""
     workspace_id = ""
@@ -147,16 +123,16 @@ def main():
     workflow_status_sleep_timer = 60 * 2
     
     try:
-        (workspace_id, workspace_name) = create_workspace(billing_project_name, bearer_token, rawls_url)
+        (workspace_id, workspace_name) = create_workspace(billing_project_name, token, rawls_url)
 
-        create_app(workspace_id, leo_url, 'WORKFLOWS_APP', 'WORKSPACE_SHARED', bearer_token)
-        workflow_app_url = poll_for_app_url(workspace_id, 'WORKFLOWS_APP', 'cbas', bearer_token, leo_url)
+        create_app(workspace_id, leo_url, 'WORKFLOWS_APP', 'WORKSPACE_SHARED', token)
+        workflow_app_url = poll_for_app_url(workspace_id, 'WORKFLOWS_APP', 'cbas', token, leo_url)
         
         if not workflow_app_url:
             raise Exception("Prerequisite workflow app URL could not be found.")
         
-        create_app(workspace_id, leo_url, 'CROMWELL_RUNNER_APP', 'USER_PRIVATE', bearer_token)
-        app_url = poll_for_app_url(workspace_id, 'CROMWELL_RUNNER_APP', 'cromwell-runner', bearer_token, leo_url)
+        create_app(workspace_id, leo_url, 'CROMWELL_RUNNER_APP', 'USER_PRIVATE', token)
+        app_url = poll_for_app_url(workspace_id, 'CROMWELL_RUNNER_APP', 'cromwell-runner', token, leo_url)
 
         if not app_url:
             raise Exception("App URL could not be found.")
@@ -170,12 +146,12 @@ def main():
         # This chunk of code supports checking one or more workflows
         # Probably won't require too much modification if we want to run additional submission tests
         workflow_ids = deque([workflow_response['id']])
-        get_completed_workflow(app_url, workflow_ids, sleep_timer=workflow_status_sleep_timer)
+        get_completed_workflow(app_url, workflow_ids, token, sleep_timer=workflow_status_sleep_timer)
     except Exception as e:
         logging.info(f"Exception occured during test:\n{e}")
         found_exception = True
     finally:
-        test_cleanup(billing_project_name, workspace_name)
+        test_cleanup(billing_project_name, workspace_name, token)
         # Use exit(1) so that GHA will fail if an exception was found during the test
         if(found_exception):
             logging.error("Workflow test failed due to exception(s)")
