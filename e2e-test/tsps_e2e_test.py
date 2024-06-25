d9324bbaa8a815175e8d9dd68c50c2cb5aebd73d
Morgan Taylor <marymorg@broadinstitute.org>
2024-06-25 13:44:07 -0400

    update teaspoons e2e test for prepare and start endpoints

--- e2e-test/tsps_e2e_test.py
+++ e2e-test/tsps_e2e_test.py
@@ -35,13 +35,9 @@ def update_imputation_pipeline_workspace_id(tsps_url, workspace_id, token):
     logging.info(f"successfully updated imputation pipeline workspace id: {workspace_id}")
 
 
-# run imputation beagle pipeline
-def run_imputation_pipeline(tsps_url, token):
+def prepare_imputation_pipeline(tsps_url, token):
     request_body = {
-        "description": "string",
-        "jobControl": {
-            "id": f'{uuid.uuid4()}'
-        },
+        "jobId": f'{uuid.uuid4()}',
         "pipelineVersion": "string",
         "pipelineInputs": {
             "multiSampleVcf": "this/is/a/fake/file.vcf.gz",
@@ -49,7 +45,34 @@ def run_imputation_pipeline(tsps_url, token):
         }
     }
 
-    uri = f"{tsps_url}/api/pipelineruns/v1/imputation_beagle"
+    uri = f"{tsps_url}/api/pipelineruns/v1/imputation_beagle/prepare"
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "accept": "application/json",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(uri, json=request_body, headers=headers)
+    status_code = response.status_code
+
+    if status_code != 200:
+        raise Exception(response.text)
+
+    logging.info(f"Successfully prepared imputation pipeline run")
+    response = json.loads(response.text)
+
+    return response['id'], response['pipelineFileInputs']
+
+# run imputation beagle pipeline
+def start_imputation_pipeline(jobId, tsps_url, token):
+    request_body = {
+        "description": "string",
+        "jobControl": {
+            "id": f'{uuid.uuid4()}'
+        }
+    }
+
+    uri = f"{tsps_url}/api/pipelineruns/v1/imputation_beagle/start"
     headers = {
         "Authorization": f"Bearer {token}",
         "accept": "application/json",
@@ -62,7 +85,7 @@ def run_imputation_pipeline(tsps_url, token):
     if status_code != 202:
         raise Exception(response.text)
 
-    logging.info(f"Successfully launched imputation pipeline")
+    logging.info(f"Successfully started imputation pipeline run")
     response = json.loads(response.text)
 
     return response['jobReport']['id']
@@ -107,6 +130,21 @@ def poll_for_imputation_job(tsps_url, job_id, token):
         poll_count -= 1
 
     raise Exception(f"tsps pipeline did not complete in 25 minutes")
+
+
+# write a hello world text file to a sas url using the azure storage library
+def upload_file_with_azcopy(sas_url):
+    blob_client = BlobClient.from_blob_url(sas_url)
+    local_file = "temp.txt"
+
+    # write the file locally
+    with open(file=local_file, mode="w") as blob_file:
+        blob_file.write("Hello, World!")
+    
+    # upload the file
+    with open(file=local_file, mode="rb") as blob_file:
+        blob_client.upload_blob(blob_file)
+
 
 # download a file from a sas url using the azure storage library
 def download_with_azcopy(sas_url):
@@ -219,9 +257,20 @@ try:
                                     "https://github.com/DataBiosphere/terra-scientific-pipelines-service/blob/main/pipelines/testing/ImputationBeagleEmpty.wdl",
                                           azure_user_token)
 
-    # launch tsps imputation pipeline
-    logging.info("running imputation pipeline")
-    job_id = run_imputation_pipeline(tsps_url, azure_user_token)
+    # prepare tsps imputation pipeline run
+    logging.info("preparing imputation pipeline run")
+    job_id, pipeline_file_inputs = prepare_imputation_pipeline(tsps_url, azure_user_token)
+
+    # make sure we got a writable sas url
+    for key, value in pipeline_file_inputs.items():
+        logging.info(f"attempting to upload a file to {key} input")
+        write_sas_url = value['sasUrl']
+        upload_file_with_azcopy(value)
+        logging.info("successfully uploaded file")
+
+    # start pipeline run
+    logging.info("starting imputation pipeline run")
+    job_id = start_imputation_pipeline(job_id, tsps_url, azure_user_token)
 
     # poll for imputation pipeline
     logging.info("polling for imputation pipeline")
@@ -231,7 +280,9 @@ try:
     for key, value in pipeline_output.items():
         logging.info(f"attempting to retrieve {key} output")
         download_with_azcopy(value)
+        logging.info("successfully downloaded file")
 
+    logging.info("TEST COMPLETE")
 
 except Exception as e:
     logging.error(f"Exception(s) occurred during test. Details: {e}")
