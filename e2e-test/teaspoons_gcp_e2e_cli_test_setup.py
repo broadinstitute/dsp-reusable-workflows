4c101c7ec103d97adffddb5bed0e292ad0e48231
M. Morgan Aster <marymorg@broadinstitute.org>
2024-12-16 13:06:23 -0500

    TSPS-331 Add teaspoons CLI e2e test (#59)
    add CLI e2e test for teaspoons

---
+++ e2e-test/teaspoons_gcp_e2e_cli_test_setup.py
@@ -0,0 +1,123 @@
+from workspace_helper import create_gcp_workspace, delete_workspace, share_workspace_grant_owner, add_wdl_to_gcp_workspace
+from helper import create_gcp_billing_project, delete_gcp_billing_project
+from teaspoons_helper import create_and_populate_terra_group, update_imputation_pipeline_workspace
+
+import os
+import logging
+
+
+# Setup configuration
+# The environment variables are injected as part of the e2e test setup which does not pass in any args
+admin_token = os.environ.get("ADMIN_TOKEN") # admin user who has access to the terra billing project
+ 
+# e2e test is using the teaspoons qa service account
+teaspoons_sa_email = "teaspoons-qa@broad-dsde-qa.iam.gserviceaccount.com"
+
+bee_name = os.environ.get("BEE_NAME")
+env_string = bee_name + ".bee.envs-terra.bio"
+
+billing_account_name = os.environ.get("BILLING_ACCOUNT_NAME")
+billing_project_name = os.environ.get("BILLING_PROJECT_NAME")
+workspace_name = os.environ.get("WORKSPACE_NAME")
+
+rawls_url = f"https://rawls.{env_string}"
+firecloud_orch_url = f"https://firecloudorch.{env_string}"
+teaspoons_url = f"https://teaspoons.{env_string}"
+
+wdl_method_version = os.environ.get("WDL_METHOD_VERSION")
+
+# configure logging format
+LOG_FORMAT = "%(asctime)s %(levelname)-8s %(message)s"
+LOG_LEVEL = "INFO"
+LOG_DATEFORMAT = "%Y-%m-%d %H:%M:%S"
+logging.basicConfig(
+    format=LOG_FORMAT,
+    level=getattr(logging, LOG_LEVEL),
+    datefmt=LOG_DATEFORMAT,
+)
+
+# ---------------------- Start Teaspoons GCP CLI E2E test SETUP ----------------------
+try:
+    logging.info("Starting Teaspoons GCP E2E test...")
+    logging.info(f"billing project: {billing_project_name}, env_string: {env_string}")
+
+    # Create Terra billing project
+    logging.info("Creating billing project...")
+    create_gcp_billing_project(rawls_url, billing_project_name, billing_account_name, admin_token)
+
+    # Create auth domain group and add Teaspoons SA as an admin
+    logging.info("Creating auth domain...")
+    auth_domain_name = "teaspoons-imputation-e2e-test"
+    group_admins = [teaspoons_sa_email]
+    create_and_populate_terra_group(firecloud_orch_url, auth_domain_name, group_admins, [], admin_token)
+
+    # Create workspace
+    logging.info("Creating workspace...")
+    workspace_name = create_gcp_workspace(
+        billing_project_name, 
+        admin_token, 
+        rawls_url, 
+        workspace_name, 
+        auth_domains=[auth_domain_name], 
+        enhanced_bucket_logging=True)
+
+    # share created workspace with the teaspoons service account
+    logging.info("sharing workspace with teaspoons qa service account")
+    share_workspace_grant_owner(firecloud_orch_url, billing_project_name, workspace_name,
+                                teaspoons_sa_email, admin_token)
+
+    # create a new imputation method that teaspoons will run
+    logging.info("creating imputation method")
+    wdl_namespace = billing_project_name
+    wdl_name = "ImputationBeagle"
+    root_entity_type = "array_imputation"
+    method_definition_dict = {
+        "methodUri": f"dockstore://github.com%2FDataBiosphere%2Fterra-scientific-pipelines-service%2FImputationBeagleEmpty/{wdl_method_version}",
+        "sourceRepo": "dockstore",
+        "methodPath": "github.com/DataBiosphere/terra-scientific-pipelines-service/ImputationBeagleEmpty",
+        "methodVersion": wdl_method_version
+    }
+    add_wdl_to_gcp_workspace(billing_project_name, workspace_name, wdl_namespace, wdl_name, method_definition_dict, root_entity_type, {}, {}, firecloud_orch_url, admin_token)
+
+    # create a new quota consumed method that teaspoons will run
+    logging.info("creating quota consumed method")
+    wdl_name = "QuotaConsumed"
+    method_definition_dict = {
+        "methodUri": f"dockstore://github.com%2FDataBiosphere%2Fterra-scientific-pipelines-service%2FQuotaConsumedEmpty/{wdl_method_version}",
+        "sourceRepo": "dockstore",
+        "methodPath": "github.com/DataBiosphere/terra-scientific-pipelines-service/QuotaConsumedEmpty",
+        "methodVersion": wdl_method_version
+    }
+    add_wdl_to_gcp_workspace(billing_project_name, workspace_name, wdl_namespace, wdl_name, method_definition_dict, root_entity_type, {}, {}, firecloud_orch_url, admin_token)
+
+    # use admin endpoint to set imputation workspace info
+    logging.info("updating imputation workspace info")
+    update_imputation_pipeline_workspace(teaspoons_url, billing_project_name, workspace_name, wdl_method_version, admin_token)
+
+    logging.info("SETUP COMPLETE")
+
+except Exception as e:
+    logging.error(f"Exception(s) occurred during setup. Details: {e}")
+
+    # delete workspace
+    logging.info("Starting workspace cleanup as part of e2e test...")
+    try:
+        delete_workspace(billing_project_name, workspace_name, rawls_url, admin_token)
+        logging.info("Workspace cleanup complete")
+    # Catch the exception and continue with the test since we don't want cleanup to affect the test results.
+    # We can assume that Janitor will clean up the workspace if the test fails
+    except Exception as e:
+        logging.warning(f"Error cleaning up workspace, test script will continue. Error details: {e}")
+
+    # delete billing project
+    logging.info("Starting billing project cleanup as part of e2e test...")
+    try:
+        delete_gcp_billing_project(rawls_url, billing_project_name, admin_token)
+        logging.info("Billing project cleanup complete")
+    # Catch the exception and continue with the test since we don't want cleanup to affect the test results.
+    # We can assume that Janitor will clean up the billing project if the test fails
+    except Exception as e:
+        logging.warning(f"Error cleaning up billing project, test script will continue. Error details: {e}")
+
+    # Use exit(1) so that GHA will fail if an exception was found during the test
+    exit(1)
--- e2e-test/teaspoons_helper.py
+++ e2e-test/teaspoons_helper.py
@@ -1,15 +1,20 @@
-from workspace_helper import create_gcp_workspace, delete_workspace, share_workspace_grant_owner, add_wdl_to_gcp_workspace
-from helper import create_gcp_billing_project, delete_gcp_billing_project
-
+import logging
 import requests
-import os
 import json
 import uuid
 import time
-import logging
 import tempfile
 import urllib.parse
 
+# configure logging format
+LOG_FORMAT = "%(asctime)s %(levelname)-8s %(message)s"
+LOG_LEVEL = "INFO"
+LOG_DATEFORMAT = "%Y-%m-%d %H:%M:%S"
+logging.basicConfig(
+    format=LOG_FORMAT,
+    level=getattr(logging, LOG_LEVEL),
+    datefmt=LOG_DATEFORMAT,
+)
 
 # update workspace id for imputation beagle pipeline
 def update_imputation_pipeline_workspace(teaspoons_url, workspace_project, workspace_name, wdl_method_version, token):
@@ -43,8 +48,7 @@ def prepare_imputation_pipeline(teaspoons_url, token):
         "pipelineInputs": {
             "multiSampleVcf": "this/is/a/fake/file.vcf.gz",
             "outputBasename": "fake_basename"
-        },
-        "description": f"e2e test run"
+        }
     }
 
     uri = f"{teaspoons_url}/api/pipelineruns/v1/prepare"
@@ -68,6 +72,7 @@ def prepare_imputation_pipeline(teaspoons_url, token):
 # run imputation beagle pipeline
 def start_imputation_pipeline(jobId, teaspoons_url, token):
     request_body = {
+        "description": f"e2e test run for jobId {jobId}",
         "jobControl": {
             "id": f'{jobId}'
         }
@@ -227,164 +232,3 @@ def add_member_to_terra_group(orch_url, group_name, email_address, role, token):
         raise Exception(response.text)
     
     logging.info(f"added {email_address} as {role} to Terra group {group_name}")
-
-
-# Setup configuration
-# The environment variables are injected as part of the e2e test setup which does not pass in any args
-admin_token = os.environ.get("ADMIN_TOKEN") # admin user who has access to the terra billing project
-user_token = os.environ.get("USER_TOKEN") # the user who will kick off the teaspoons job
- 
-# e2e test is using the teaspoons qa service account
-teaspoons_sa_email = "teaspoons-qa@broad-dsde-qa.iam.gserviceaccount.com"
-
-bee_name = os.environ.get("BEE_NAME")
-env_string = bee_name + ".bee.envs-terra.bio"
-
-billing_account_name = os.environ.get("BILLING_ACCOUNT_NAME")
-billing_project_name = os.environ.get("BILLING_PROJECT_NAME")
-workspace_name = ""
-
-rawls_url = f"https://rawls.{env_string}"
-firecloud_orch_url = f"https://firecloudorch.{env_string}"
-teaspoons_url = f"https://teaspoons.{env_string}"
-
-wdl_method_version = os.environ.get("WDL_METHOD_VERSION")
-
-# configure logging format
-LOG_FORMAT = "%(asctime)s %(levelname)-8s %(message)s"
-LOG_LEVEL = "INFO"
-LOG_DATEFORMAT = "%Y-%m-%d %H:%M:%S"
-logging.basicConfig(
-    format=LOG_FORMAT,
-    level=getattr(logging, LOG_LEVEL),
-    datefmt=LOG_DATEFORMAT,
-)
-
-# ---------------------- Start Teaspoons GCP E2E test ----------------------
-found_exception = False
-try:
-    logging.info("Starting Teaspoons GCP E2E test...")
-    logging.info(f"billing project: {billing_project_name}, env_string: {env_string}")
-
-    # Create Terra billing project
-    logging.info("Creating billing project...")
-    create_gcp_billing_project(rawls_url, billing_project_name, billing_account_name, admin_token)
-
-    # Create auth domain group and add Teaspoons SA as an admin
-    logging.info("Creating auth domain...")
-    auth_domain_name = "teaspoons-imputation-e2e-test"
-    group_admins = [teaspoons_sa_email]
-    create_and_populate_terra_group(firecloud_orch_url, auth_domain_name, group_admins, [], admin_token)
-
-    # Create workspace
-    logging.info("Creating workspace...")
-    workspace_name = create_gcp_workspace(
-        billing_project_name, 
-        admin_token, 
-        rawls_url, 
-        workspace_name, 
-        auth_domains=[auth_domain_name], 
-        enhanced_bucket_logging=True)
-
-    # share created workspace with the teaspoons service account
-    logging.info("sharing workspace with teaspoons qa service account")
-    share_workspace_grant_owner(firecloud_orch_url, billing_project_name, workspace_name,
-                                teaspoons_sa_email, admin_token)
-
-    # create a new imputation method that teaspoons will run
-    logging.info("creating imputation method")
-    wdl_namespace = billing_project_name
-    wdl_name = "ImputationBeagle"
-    root_entity_type = "array_imputation"
-    method_definition_dict = {
-        "methodUri": f"dockstore://github.com%2FDataBiosphere%2Fterra-scientific-pipelines-service%2FImputationBeagleEmpty/{wdl_method_version}",
-        "sourceRepo": "dockstore",
-        "methodPath": "github.com/DataBiosphere/terra-scientific-pipelines-service/ImputationBeagleEmpty",
-        "methodVersion": wdl_method_version
-    }
-    add_wdl_to_gcp_workspace(billing_project_name, workspace_name, wdl_namespace, wdl_name, method_definition_dict, root_entity_type, {}, {}, firecloud_orch_url, admin_token)
-
-    # create a new quota consumed method that teaspoons will run
-    logging.info("creating quota consumed method")
-    wdl_name = "QuotaConsumed"
-    method_definition_dict = {
-        "methodUri": f"dockstore://github.com%2FDataBiosphere%2Fterra-scientific-pipelines-service%2FQuotaConsumedEmpty/{wdl_method_version}",
-        "sourceRepo": "dockstore",
-        "methodPath": "github.com/DataBiosphere/terra-scientific-pipelines-service/QuotaConsumedEmpty",
-        "methodVersion": wdl_method_version
-    }
-    add_wdl_to_gcp_workspace(billing_project_name, workspace_name, wdl_namespace, wdl_name, method_definition_dict, root_entity_type, {}, {}, firecloud_orch_url, admin_token)
-
-    # use admin endpoint to set imputation workspace info
-    logging.info("updating imputation workspace info")
-    update_imputation_pipeline_workspace(teaspoons_url, billing_project_name, workspace_name, wdl_method_version, admin_token)
-
-    # query for user quota consumed before running pipeline, expect the default of 0
-    assert 0 == query_for_user_quota_consumed(teaspoons_url, user_token)
-
-    # prepare teaspoons imputation pipeline run
-    logging.info("preparing imputation pipeline run")
-    job_id, pipeline_file_inputs = prepare_imputation_pipeline(teaspoons_url, user_token)
-
-    # make sure we got a writable signed url
-    for key, value in pipeline_file_inputs.items():
-        logging.info(f"attempting to upload a file to {key} input")
-        upload_file_with_signed_url(value['signedUrl'])
-        logging.info("successfully uploaded file")
-
-    # start pipeline run
-    logging.info("starting imputation pipeline run")
-    job_id_from_start_run, result_url = start_imputation_pipeline(job_id, teaspoons_url, user_token)
-
-    assert(job_id == job_id_from_start_run)
-
-    # poll for imputation pipeline
-    logging.info("polling for imputation pipeline")
-    pipeline_output = poll_for_imputation_job(result_url, user_token)
-
-    # grab data using signed url
-    for key, value in pipeline_output.items():
-        logging.info(f"attempting to retrieve {key} output")
-        download_with_signed_url(value)
-        logging.info("successfully downloaded file")
-
-    # query for user quota consumed after running pipeline, expect an increase of 500
-    # because that is the min quota consumed for array_imputation
-    assert 500 == query_for_user_quota_consumed(teaspoons_url, user_token)
-
-    logging.info("TEST COMPLETE")
-
-except Exception as e:
-    logging.error(f"Exception(s) occurred during test. Details: {e}")
-    found_exception = True
-finally:
-    # delete workspace
-    logging.info("Starting workspace cleanup as part of e2e test...")
-    try:
-        delete_workspace(billing_project_name, workspace_name, rawls_url, admin_token)
-        logging.info("Workspace cleanup complete")
-    # Catch the exception and continue with the test since we don't want cleanup to affect the test results.
-    # We can assume that Janitor will clean up the workspace if the test fails
-    # TODO: Instead of catching exception and continuing with test, the script should fail the test once
-    #       https://broadworkbench.atlassian.net/browse/WOR-1309 is fixed
-    except Exception as e:
-        logging.warning(f"Error cleaning up workspace, test script will continue. Error details: {e}")
-
-    # delete billing project
-    logging.info("Starting billing project cleanup as part of e2e test...")
-    try:
-        delete_gcp_billing_project(rawls_url, billing_project_name, admin_token)
-        logging.info("Billing project cleanup complete")
-    # Catch the exception and continue with the test since we don't want cleanup to affect the test results.
-    # We can assume that Janitor will clean up the billing project if the test fails
-    # TODO: Instead of catching exception and continuing with test, the script should fail the test once
-    #       https://broadworkbench.atlassian.net/browse/WOR-1309 is fixed
-    except Exception as e:
-        logging.warning(f"Error cleaning up billing project, test script will continue. Error details: {e}")
-
-    # Use exit(1) so that GHA will fail if an exception was found during the test
-    if found_exception:
-        logging.error("Exceptions found during test run. Test failed")
-        exit(1)
-    else:
-        logging.info("Test completed successfully")
