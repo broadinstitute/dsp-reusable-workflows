d6fa2c2876ed9f47895a63292a151653f5703df9
Saloni Shah <salonipshah11@gmail.com>
2026-02-11 18:19:37 -0500

    last few changes

--- e2e-test/teaspoons_helper.py
+++ e2e-test/teaspoons_helper.py
@@ -17,6 +17,7 @@ logging.basicConfig(
     format=LOG_FORMAT,
     level=getattr(logging, LOG_LEVEL),
     datefmt=LOG_DATEFORMAT,
+    force=True  # override any existing logging configuration
 )
 
 # update workspace id for imputation beagle pipeline
